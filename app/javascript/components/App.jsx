0eceb5e6ceb4995373f4915101391a90d7af4232
Karan Sapolia <sapoliakaran@gmail.com>
2022-01-02 14:56:43 +0530

    Speed up esbuild recompiles with tailwind
    Mimicked changes made by Paco in Brian Lovin's personal
    website to speed up local development re-compilation of
    tailwind stylesheets by extracting tailwind utilities
    to another css file and importing it in App.jsx
    
    reference: https://github.com/brianlovin/briOS/pull/1193

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Calendar from "./Calendar";
 
+import "../../styles/tailwind-utils.css";
+
 const Homepage = () => {
   return (
     <div className="text-3xl text-blue-600 font-extrabold">
