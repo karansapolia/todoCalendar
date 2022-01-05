5a73cfeb92fab6163d50af91b45a217a05def280
Karan Sapolia <sapoliakaran@gmail.com>
2022-01-06 02:40:10 +0530

    Add graphQL to todoCal

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :todos
   root 'homepage#index'
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
+
+  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
